0c9c2b83b945a3be089e67f9a62f92c9e51f2ac5
Birkin James Diana <birkin.diana@gmail.com>
2023-03-06 10:08:21 -0500

    outlines main flow.

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Controller.
+"""
+
+import trio
+
+## initiate a mysql dump
+
+
+## evaluate if there have been any changes
+"""
+If some aspect of the dump contains a time-stamp, see if there's a way to determine whether the actual data has changed.
+"""
+
+## if the data has changed...
+
+    ## commit to repo-A
+
+    ## push to repo-A
+
+    ## commit to repo-B
+
+    ## push to repo-B
+
+
+if __name__ == '__main__':
+    trio.run( main )
