ce1e6a9b6510111c0eb4b31d0bf18b5b22f3284e
Birkin James Diana <birkin.diana@gmail.com>
2023-07-06 09:25:44 -0400

    stubs all steps.

--- main.py
+++ main.py
@@ -30,25 +30,30 @@ def manager():
     (ok , err) = validate_env_vars()
 
     ## TODO-determine whether to run script -------------------------
-    # (continue, err) = determine_whether_to_run_script()
+    (continue_processing, err) = determine_whether_to_run_script()  # TODO; hard-coded to True for now
 
-    ## initiate a mysql dump --
-    HEREZZ
+    ## initiate a mysql dump ----------------------------------------
+    (sql_filepath, err) = initiate_mysql_dump()
 
-    ## evaluate if there have been any changes
-    """
-    If some aspect of the dump contains a time-stamp, see if there's a way to determine whether the actual data has changed.
-    """
+    ## evaluate if there have been any changes ----------------------
+    (changes_detected, err) = look_for_changes( sql_filepath )  # TODO; hard-coded to True for now
 
+    ## update repos -------------------------------------------------
     ## if the data has changed...
+    continue_flow = True
+    if continue_flow:
 
         ## commit to repo-A
+        (ok, err) = commit_to_repo_A()
 
         ## push to repo-A
+        (ok, err) = push_to_repo_A()
 
         ## commit to repo-B
+        (ok, err) = commit_to_repo_B()
 
         ## push to repo-B
+        (ok, err) = push_to_repo_B()
 
     ## end def manager()
 
